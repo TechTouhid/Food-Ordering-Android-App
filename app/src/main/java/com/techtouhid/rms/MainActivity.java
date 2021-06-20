4c1f5e486271af8a678cc0ea11e141c2c7ce1586
Touhidalamin <touhidalamin123@gmail.com>
2021-06-21 00:56:11 +0600

    created main interface

--- app/src/main/java/com/techtouhid/rms/MainActivity.java
+++ app/src/main/java/com/techtouhid/rms/MainActivity.java
@@ -2,12 +2,35 @@ package com.techtouhid.rms;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.techtouhid.rms.adapters.MainAdapter;
+import com.techtouhid.rms.databinding.ActivityMainBinding;
+import com.techtouhid.rms.models.MainModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Implemented view binding method
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<MainModel> list = new ArrayList<>();
+
+        list.add(new MainModel(R.drawable.barger, "Burger", "5", "Chicken burger with extra sos"));
+        list.add(new MainModel(R.drawable.pitzza, "Pizza", "10", "Pizza burger with extra sos"));
+        list.add(new MainModel(R.drawable.chinese, "Chinese", "15", "Chinese burger with extra sos"));
+
+        MainAdapter adapter = new MainAdapter(list, this);
+        binding.recyclerview.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
+
     }
 }
